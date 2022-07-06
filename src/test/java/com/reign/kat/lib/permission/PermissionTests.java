4d6cdbf67c449b6dd1608a290c7aaa49645a12f3
ReignBit <bitbytegaming@gmail.com>
2022-07-06 21:50:37 +0100

    :white_check_mark: Example test

---
+++ src/test/java/com/reign/kat/lib/permission/PermissionTests.java
@@ -0,0 +1,15 @@
+package com.reign.kat.lib.permission;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class PermissionTests {
+    @Test
+    public void testTest()
+    {
+        assertEquals(1,1);
+    }
+
+}
