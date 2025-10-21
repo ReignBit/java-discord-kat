9a68594d26c7b025247bb8ab6ba8cc79c457f91b
ReignBit <bitbytegaming@gmail.com>
2025-10-21 03:36:35 +0100

    Add auto skipping non-music sections
    Will be worked on in the future to add customization options such as enabling/disabling and which categories of segments to skip

--- src/main/java/com/reign/kat/lib/voice/newvoice/RequestedTrack.java
+++ src/main/java/com/reign/kat/lib/voice/newvoice/RequestedTrack.java
@@ -1,14 +1,20 @@
 package com.reign.kat.lib.voice.newvoice;
 
+import com.reign.api.sponsorblock.SkipSegment;
+import com.reign.api.sponsorblock.SponsorblockAPI;
 import com.reign.kat.lib.snowflake.MemberId;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.TrackMarker;
 import net.dv8tion.jda.api.entities.Member;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static com.reign.kat.lib.utils.Utilities.timeConversion;
 
 
 public class RequestedTrack
 {
+    private static final Logger log = LoggerFactory.getLogger(RequestedTrack.class);
     public final AudioTrack track;
 
     public final MemberId requester;
@@ -18,6 +24,8 @@ public class RequestedTrack
     public final String title;
     public final String author;
     public final String url;
+    public final String videoID; // Only for YouTube
+    public final boolean isYoutube;
 
     public RequestedTrack(Member member, AudioTrack track)
     {
@@ -30,6 +38,15 @@ public class RequestedTrack
         title = track.getInfo().title;
         author = track.getInfo().author;
         url = track.getInfo().uri;
+
+        /* Sponsorblock video ID */
+        isYoutube = url.toLowerCase().contains("youtube") || url.toLowerCase().contains("youtu.be");
+        if (isYoutube) {
+            videoID = url.split("\\?v=")[1].split("&")[0];
+            loadVideoSkipSegments();
+        } else {
+            videoID = null;
+        }
     }
 
     public RequestedTrack(MemberId member, AudioTrack track)
@@ -43,6 +60,15 @@ public class RequestedTrack
         title = track.getInfo().title;
         author = track.getInfo().author;
         url = track.getInfo().uri;
+
+        /* Sponsorblock video ID */
+        isYoutube = url.toLowerCase().contains("youtube") || url.toLowerCase().contains("youtu.be");
+        if (isYoutube) {
+            videoID = url.split("v\\?=")[1].split("&")[0];
+            loadVideoSkipSegments();
+        } else {
+            videoID = null;
+        }
     }
 
     /**
@@ -72,5 +98,19 @@ public class RequestedTrack
     public RequestedTrack copy()
     {
         return new RequestedTrack(requester, track.makeClone());
+    }
+
+
+    private void loadVideoSkipSegments() {
+        SkipSegment[] skipSegments = SponsorblockAPI.getSkipSegments(videoID);
+        if (skipSegments == null) { return; }
+
+        for (SkipSegment ss : skipSegments) {
+            track.addMarker(new TrackMarker(
+                    (long)(ss.start * 1000) + 20,   // Add a 20ms delay so we are guaranteed to hit when pos = 0
+                    new SponsorblockTrackMarkerHandler(this, ss)
+            ));
+            log.debug("Added skipsegment: {}, {} -> {}", videoID, (long)(ss.start * 1000), ss.stop);
+        }
     }
 }
